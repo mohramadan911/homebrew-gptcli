a1e4e1411024e042eb0621cf8a5fccfd044aba0b
mohramadan911 <mohamed.issa@new-work.se>
2024-02-14 23:49:01 +0100

    new sha

--- Formula/gptcli.rb
+++ Formula/gptcli.rb
@@ -1,8 +1,8 @@
-class gptcli < Formula
-  desc "your cli as a gpt"
+class Gptcli < Formula
+  desc "Your CLI as a GPT"
   homepage "https://github.com/mohramadan911/gptcli"
-  url "https://github.com/mohramadan911/gptcli/get/HEAD.zip", :using => :curl
-
+  url "https://github.com/mohramadan911/gptcli/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "4e9cbd5910c1aa5618d08a5ea4815cd9c3861461a42a52064b049ca38323a975"
   def install
     bin.install "bin/gptcli"
   end
