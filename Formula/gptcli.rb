b8639377233a26fe2e5442c72fea90cf706108e1
mohramadan911 <mohamed.issa@new-work.se>
2024-02-15 15:59:44 +0100

    fix tar file

--- Formula/gptcli.rb
+++ Formula/gptcli.rb
@@ -1,7 +1,7 @@
 class Gptcli < Formula
   desc "Your CLI as a GPT"
   homepage "https://github.com/mohramadan911/gptcli"
-  url "https://github.com/mohramadan911/gptcli/archive/refs/tags/v1.2.0tar.gz"
+  url "https://github.com/mohramadan911/gptcli/archive/refs/tags/v1.2.0.tar.gz"
   sha256 "fa3cfd0bf337ca10c198c8816fc79e76b2bbce7d5a5ece004820d706a0b78a1d"
   def install
     bin.install "bin/gptcli"
