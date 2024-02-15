1b5b0ba5cc506397622724c2a5d5c3fffa3dcb72
mohramadan911 <mohamed.issa@new-work.se>
2024-02-15 14:41:00 +0100

    sha

--- Formula/gptcli.rb
+++ Formula/gptcli.rb
@@ -2,7 +2,7 @@ class Gptcli < Formula
   desc "Your CLI as a GPT"
   homepage "https://github.com/mohramadan911/gptcli"
   url "https://github.com/mohramadan911/gptcli/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "4e9cbd5910c1aa5618d08a5ea4815cd9c3861461a42a52064b049ca38323a975"
+  sha256 "fabf294339fbe83c3dfce0557d31ca962ef8a20b112db03c3caa49705db920bb"
   def install
     bin.install "bin/gptcli"
   end
