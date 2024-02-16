8b7884ce66f5a203366f3b8ec5465b0c320cc038
mohramadan911 <mohamed.issa@new-work.se>
2024-02-16 18:47:03 +0100

    cleanup

--- Formula/gptcli.rb
+++ Formula/gptcli.rb
@@ -3,8 +3,6 @@ class Gptcli < Formula
   homepage "https://github.com/mohramadan911/gptcli"
   url "https://github.com/mohramadan911/gptcli/archive/refs/tags/v1.2.8.tar.gz"
   sha256 "0e43c8b986bfcd4798ff90e97732186abd91d7e4c1ddf6adb06b3498d9a1d276"
-# Minitest::Assertion: Expected /gptcli\ 1\.2\.7/ to match "gptcli Unknown version\n".
-  version "1.2.7"
   def install
     bin.install "bin/gptcli"
     bin.install "VERSION"
