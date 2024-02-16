1584e321c84c477eae1286986be28fe681602de7
mohramadan911 <mohamed.issa@new-work.se>
2024-02-16 18:13:14 +0100

    bump to 1.2.7

--- Formula/gptcli.rb
+++ Formula/gptcli.rb
@@ -1,8 +1,8 @@
 class Gptcli < Formula
   desc "Your CLI as a GPT"
   homepage "https://github.com/mohramadan911/gptcli"
-  url "https://github.com/mohramadan911/gptcli/archive/refs/tags/v1.2.6.tar.gz"
-  sha256 "683965b20c5894b81df93fd3430e6a1ad504052cd29baebc8a482910c0c15636"
+  url "https://github.com/mohramadan911/gptcli/archive/refs/tags/v1.2.7.tar.gz"
+  sha256 "84aa0b94d88df3b0f35088118d9d1479d3c4fb0236c8ba131a7996edbae6ba33"
 
   def install
     bin.install "bin/gptcli"
