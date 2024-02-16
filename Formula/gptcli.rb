94a944c61c52a9491dc3cd807878a609a17b12be
mohramadan911 <mohamed.issa@new-work.se>
2024-02-16 19:31:43 +0100

    fix whitespace

--- Formula/gptcli.rb
+++ Formula/gptcli.rb
@@ -3,7 +3,6 @@ class Gptcli < Formula
   homepage "https://github.com/mohramadan911/gptcli"
   url "https://github.com/mohramadan911/gptcli/archive/refs/tags/v1.2.13.tar.gz"
   sha256 "994fa23d12d5b454333fa6bb1aa89e6fc2f749c4ce0afd12e2e36e81261f8781"
-  
   def install
     bin.install "bin/gptcli"
     # Removed the bin.install "VERSION" line
