e0bd24543a49a5a202d31b974c6edf5a82f8d522
mohramadan911 <mohamed.issa@new-work.se>
2024-02-16 19:58:23 +0100

    bump version to 1.2.14

--- Formula/gptcli.rb
+++ Formula/gptcli.rb
@@ -1,8 +1,8 @@
 class Gptcli < Formula
   desc "Your CLI as a GPT"
   homepage "https://github.com/mohramadan911/gptcli"
-  url "https://github.com/mohramadan911/gptcli/archive/refs/tags/v1.2.13.tar.gz"
-  sha256 "994fa23d12d5b454333fa6bb1aa89e6fc2f749c4ce0afd12e2e36e81261f8781"
+  url "https://github.com/mohramadan911/gptcli/archive/refs/tags/v1.2.14.tar.gz"
+  sha256 "8f5280cd5b5b494d8b8a9b5ce73e74d5822c99ed6e6c18b6ce54349d768eadde"
   def install
     bin.install "bin/gptcli"
     # Removed the bin.install "VERSION" line
